d88dd57a5b5ddc6ac3106f16354c4d74cf8cdeea
rosenberg9 <121764571+rosenberg9@users.noreply.github.com>
2025-04-23 21:38:57 +0000

    Deploying to gh-pages from @ rosenberg9/rosenberg9.github.io@55f0e9bb1f2a704d871a60b1819813774f3a082b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is my CV. For the most updated version, click the icon above and download the PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
