6ed5df4e24618de84b9429d4412df352fffcb7a7
rosenberg9 <121764571+rosenberg9@users.noreply.github.com>
2025-12-01 15:52:58 +0000

    Deploying to gh-pages from @ rosenberg9/rosenberg9.github.io@da3e7adcc539bc74df6f2975b5ee70daad6deebe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "Teaching",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-contact",
           title: "Contact",
           description: "",
@@ -77,6 +70,16 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
+            },},{id: "projects-experience-as-a-teaching-assistant",
+          title: 'Experience as a teaching assistant',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-introduction-to-machine-learning-tutorial",
+          title: 'Introduction to machine learning tutorial',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
